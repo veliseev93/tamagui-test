3f7cb26c7934eebe5a2e416c991595c2acc88521
Vasily Eliseev <veliseev@ronasit.com>
2024-10-16 14:54:23 +0600

    feat: add next js app

---
+++ types.d.ts
@@ -0,0 +1,4 @@
+// This file is added for correct work of TS-checks in pre-commit hook using tsc-files
+// Issue: https://github.com/gustavopch/tsc-files/issues/20
+// TODO: Uncomment the line below if you have an Expo app
+// import 'node_modules/@nx/expo/typings/svg.d.ts';
